ad0605ab5062526729003af019b2423a0b4deb2f
Comfortley <comfortley66@gmail.com>
2020-12-10 11:00:21 +0500

    New Admin Model

--- lib/admin.dart
+++ lib/admin.dart
@@ -9,7 +9,6 @@ class AdminLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Data.primaryColor,
       appBar: AppBar(
         backgroundColor: Data.primaryColor,
         title:Text(
@@ -76,164 +75,211 @@ class _AdminLoginBodyState extends State<AdminLoginBody> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SafeArea(
-        child: SingleChildScrollView(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.only(top: 10.0,left: 15.0,right: 15.0,bottom: 0.0),
-                  child: TextFormField(
-                    validator: (String email) {
-                      if (email.isEmpty || email == null) {
-                        return "Email can't be empty!";
-                      }
-                      else if(!(email.contains("@"))) {
-                        return "Please enter a valid email!";
-                      }
-                      else if (!(email.contains(".com"))) {
-                        return "Please enter a valid email!";
-                      }
-                      return null;
-                    },
-                    controller: emailController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      labelText: "Email:",
-                      labelStyle: TextStyle(
-                        color: Colors.black,
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Stack(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset(
+                      "assets/login_background.png",
+                      // width: MediaQuery.of(context).size.width,
+                      // fit: BoxFit.fill,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 60),
+                    alignment: Alignment.topCenter,
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Data.secondaryColor,
+                      child: Image.asset('assets/logo.png'),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10.0,left: 40.0,right: 40.0,bottom: 0.0),
+                child: TextFormField(
+                  validator: (String email) {
+                    if (email.isEmpty || email == null) {
+                      return "Email can't be empty!";
+                    }
+                    else if(!(email.contains("@"))) {
+                      return "Please enter a valid email!";
+                    }
+                    else if (!(email.contains(".com"))) {
+                      return "Please enter a valid email!";
+                    }
+                    return null;
+                  },
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: "Email:",
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.cyan.shade300,
+                        width: 1.5,
                       ),
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Data.primaryColor,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.cyan.shade300,
+                        width: 1.5,
                       ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.cyan.shade300,
-                          width: 1.5,
-                        ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.cyan.shade300,
+                        width: 1.5,
                       ),
-                      border: OutlineInputBorder(),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Data.primaryColor,
                     ),
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.only(top: 10.0,left: 15.0,right: 15.0,bottom: 0.0),
-                  child: TextFormField(
-                    autofocus: false,
-                    validator: (String password) {
-                      if (password.isEmpty || password == null) {
-                        return "Password can't be empty!";
-                      }
-                      else if(password.length < 8) {
-                        return "Length of password must be equal or greater than 8!";
-                      }
-                      return null;
-                    },
-                    controller: passwordController,
-                    obscureText: passwordNotVisible,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      labelText: "Password:",
-                      labelStyle: TextStyle(
-                        color: Colors.black,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.cyan.shade300,
-                          width: 1.5,
-                        ),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Data.primaryColor,
-                      ),
-                      suffixIcon: InkWell(
-                        onTap: () {
-                          setState(() {
-                            passwordNotVisible = !passwordNotVisible;
-                          });
-                        },
-                        child: Icon(
-                          (passwordNotVisible)?EvaIcons.eye:EvaIcons.eyeOff,
-                          color: Data.primaryColor,
-                        ),
-                      ),
-                      border: OutlineInputBorder(),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10.0,left: 40.0,right: 40.0,bottom: 0.0),
+                child: TextFormField(
+                  autofocus: false,
+                  validator: (String password) {
+                    if (password.isEmpty || password == null) {
+                      return "Password can't be empty!";
+                    }
+                    else if(password.length < 8) {
+                      return "Length of password must be equal or greater than 8!";
+                    }
+                    return null;
+                  },
+                  controller: passwordController,
+                  obscureText: passwordNotVisible,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    labelText: "Password:",
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
                     ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 15.0,left: 15.0,right: 15.0,bottom: 0.0),
-                  child: SizedBox(
-                    height: 45,
-                    child: RaisedButton(
-                      child: Text(
-                        "Submit",
-                        style: TextStyle(
-                          color: Data.primaryColor,
-                        ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.cyan.shade300,
+                        width: 1.5,
                       ),
-                      color: Data.secondaryColor,
-                      textColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: BorderSide(
-                          color: Data.primaryColor,
-                          width: 2.5,
-                        ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.cyan.shade300,
+                        width: 1.5,
                       ),
-                      onPressed: () async {
-                        String email = emailController.text;
-                        String password = passwordController.text;
-                        if(_formKey.currentState.validate()) {
-                          if (email == adminEmail && password == adminPassword) {
-                            print('success!');
-                            emailController.text = '';
-                            passwordController.text = '';
-                            Navigator.push(context, MaterialPageRoute(
-                              builder: (BuildContext context) {
-                                return AdminControl();
-                              }
-                            ));
-                          }
-                          else {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: Text('Error'),
-                                  content: Text('Email or password is wrong! Please try again.'),
-                                  actions: [
-                                    TextButton(
-                                      child: Text(
-                                        'OK',
-                                        style: TextStyle(
-                                          color: Data.primaryColor,
-                                        ),
-                                      ),
-                                      onPressed: () {
-                                        Navigator.pop(context);
-                                      },
-                                    ),
-                                  ],
-                                );
-                              }
-                            );
-                          }
-                        }
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.cyan.shade300,
+                        width: 1.5,
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Data.primaryColor,
+                    ),
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        setState(() {
+                          passwordNotVisible = !passwordNotVisible;
+                        });
                       },
+                      child: Icon(
+                        (passwordNotVisible)?EvaIcons.eye:EvaIcons.eyeOff,
+                        color: Data.primaryColor,
+                      ),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 15.0,left: 40.0,right: 40.0,bottom: 0.0),
+                child: SizedBox(
+                  height: 45,
+                  child: RaisedButton(
+                    child: Text(
+                      "Submit",
+                      style: TextStyle(
+                        color: Data.primaryColor,
+                      ),
+                    ),
+                    color: Data.secondaryColor,
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(
+                        color: Data.primaryColor,
+                        width: 2.5,
+                      ),
+                    ),
+                    onPressed: () async {
+                      String email = emailController.text;
+                      String password = passwordController.text;
+                      if(_formKey.currentState.validate()) {
+                        if (email == adminEmail && password == adminPassword) {
+                          print('success!');
+                          emailController.text = '';
+                          passwordController.text = '';
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (BuildContext context) {
+                              return AdminControl();
+                            }
+                          ));
+                        }
+                        else {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('Error'),
+                                content: Text('Email or password is wrong! Please try again.'),
+                                actions: [
+                                  TextButton(
+                                    child: Text(
+                                      'OK',
+                                      style: TextStyle(
+                                        color: Data.primaryColor,
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                ],
+                              );
+                            }
+                          );
+                        }
+                      }
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
